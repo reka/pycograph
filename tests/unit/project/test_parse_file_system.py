60beb3dabbc7cf95c334256ae22b0132f9dacec2
reka <reka@hey.com>
2021-05-03 22:47:38 +0200

    add tests
    * project dir contains a non-py file
    * delete graph at overwrite: ConnectionError

--- tests/unit/project/test_parse_file_system.py
+++ tests/unit/project/test_parse_file_system.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 
 import pytest
@@ -6,8 +7,18 @@ from pycograph.exceptions import NoPythonFileFoundException
 from pycograph.project import PythonProject
 
 
+def test_project_dir_empty():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        project = PythonProject(tmpdirname)
+        with pytest.raises(NoPythonFileFoundException):
+            project._parse_file_system()
+
+
 def test_no_python_file_in_project_dir():
     with tempfile.TemporaryDirectory() as tmpdirname:
+        file_path = os.path.join(tmpdirname, "test.txt")
+        with open(file_path, "w") as f:
+            f.write("test content")
         project = PythonProject(tmpdirname)
         with pytest.raises(NoPythonFileFoundException):
             project._parse_file_system()
