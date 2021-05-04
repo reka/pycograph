58b12d403c1c249face29e724f8e4b9963ef4dbf
reka <382113+reka@users.noreply.github.com>
2021-05-04 22:18:04 +0200

    add tests (#13)
    * package tests: add assertion about node_properties
    * add test with test_constant
    * add parse_module tests for functions
    * add parse_module tests for classes

--- tests/integration/parse_module/test_constant.py
+++ tests/integration/parse_module/test_constant.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pycograph.schemas.parse_result import ConstantWithContext
+from tests.integration.helper import create_module_with_content
 
 
 @pytest.mark.parametrize("content", ["ANSWER=42"])
@@ -13,3 +14,16 @@ def test_define_a_constant(module_with_content):
     only_content = modu.contained_objects[0]
     assert type(only_content) == ConstantWithContext
     assert only_content.name == "ANSWER"
+    assert only_content.label() == "constant"
+
+
+def test_define_a_constant_in_test_module():
+    modu = create_module_with_content("ANSWER=42", package_name="tests.unit.helpers")
+
+    modu.parse()
+
+    assert len(modu.contained_objects) == 1
+    only_content = modu.contained_objects[0]
+    assert type(only_content) == ConstantWithContext
+    assert only_content.name == "ANSWER"
+    assert only_content.label() == "test_constant"
---
+++ tests/integration/parse_module/test_function.py
@@ -0,0 +1,50 @@
+from pycograph.schemas.parse_result import FunctionWithContext
+from tests.integration.helper import create_module_with_content
+
+
+def test_define_a_function_in_production_code():
+    content = """
+def dummy():
+    pass
+"""
+    modu = create_module_with_content(content)
+
+    modu.parse()
+
+    assert len(modu.contained_objects) == 1
+    only_content = modu.contained_objects[0]
+    assert type(only_content) == FunctionWithContext
+    assert only_content.name == "dummy"
+    assert only_content.label() == "function"
+
+
+def test_define_a_test_function_in_test_module():
+    content = """
+def test_dummy():
+    pass
+"""
+    modu = create_module_with_content(content, package_name="tests.unit.helpers")
+
+    modu.parse()
+
+    assert len(modu.contained_objects) == 1
+    only_content = modu.contained_objects[0]
+    assert type(only_content) == FunctionWithContext
+    assert only_content.name == "test_dummy"
+    assert only_content.label() == "test_function"
+
+
+def test_define_a_helper_function_in_test_module():
+    content = """
+def dummy():
+    pass
+"""
+    modu = create_module_with_content(content, package_name="tests.unit.helpers")
+
+    modu.parse()
+
+    assert len(modu.contained_objects) == 1
+    only_content = modu.contained_objects[0]
+    assert type(only_content) == FunctionWithContext
+    assert only_content.name == "dummy"
+    assert only_content.label() == "test_helper_function"
